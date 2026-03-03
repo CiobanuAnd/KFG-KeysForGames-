7acc1ce76c324217c1847de5bbd9ce8d247b6bcb
Daniel <daniel.abel.arcalean@gmail.com>
2026-03-03 12:24:10 +0200

    eroare ceva

--- appSite/urls.py
+++ appSite/urls.py
@@ -1,14 +1,6 @@
 from django.urls import path
-<<<<<<< HEAD
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-=======
-
-from .views import helloword
-
-urlpatterns = [
-    path('', helloword.helloworld, name='helloworld'),
->>>>>>> 8c1b8fb664aa617b43c868b4db589ed14cac16c6
 ]
