8c1b8fb664aa617b43c868b4db589ed14cac16c6
Ciobanu Andrei <ciobanuandrei1111@gmail.com>
2026-03-03 10:47:46 +0200

    Am creat o baza de date cu numele mydb,user kfg_user si parola 1234 si am conectat aplicatia la mysql

--- KFG/settings.py
+++ KFG/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'appSite',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'KFG.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydb',
+        'USER': 'kfg_user',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
--- appSite/urls.py
+++ appSite/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views
+
+from .views import helloword
 
 urlpatterns = [
-    path('appSite/', views.helloworld, name='hello world'),
+    path('', helloword.helloworld, name='helloworld'),
 ]
