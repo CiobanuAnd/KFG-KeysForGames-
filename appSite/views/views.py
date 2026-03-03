a0686b7f72ca15b9193967e4d1edb7b075d6564d
Ciobanu Andrei <ciobanuandrei1111@gmail.com>
2026-03-03 13:47:34 +0200

    am facut url /games/ care listeaza jocurile

--- appSite/views/views.py
+++ appSite/views/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from appSite.models import Game
 
 # Create your views here.
 def home(request):
     return render(request, 'gameshop/home.html')
+
+def games_list(request):
+    all_games = Game.objects.all().select_related('buyer')
+    return render(request, 'gameshop/games.html', {'games': all_games})
