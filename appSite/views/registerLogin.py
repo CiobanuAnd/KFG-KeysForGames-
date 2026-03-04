1743e8b069af6af43c85016ab53a57399a052ea1
Ciobanu Andrei <ciobanuandrei1111@gmail.com>
2026-03-04 11:55:37 +0200

    am facut ca la register sa se adaue si in appSite_user un user

--- appSite/views/registerLogin.py
+++ appSite/views/registerLogin.py
@@ -2,11 +2,13 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
+from models import User as AppUser
 
 def register_view(request):
     if request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
+        email = request.POST["email"]
 
         if User.objects.filter(username=username).exists():
             messages.error(request, "Username already exists")
@@ -17,6 +19,13 @@ def register_view(request):
             password=password
         )
         user.save()
+
+        AppUser.objects.create(
+            username = username,
+            email = email,
+            balance = 0.0
+        )
+        messages.success(request, 'Account created succesfully!')
         return redirect("login")
 
     return render(request, "gameshop/register.html")
@@ -31,6 +40,7 @@ def login_view(request):
 
         if user is not None:
             login(request, user)
+            messages.success(request, 'Login succesfully')
             return redirect("home")
         else:
             messages.error(request, "Invalid credentials")
