7b960d81c362ea2e634b7b6cffc1cda5283abdb1
Ciobanu Andrei <ciobanuandrei1111@gmail.com>
2026-03-04 10:15:09 +0200

    fix eroare + login si register html

--- appSite/urls.py
+++ appSite/urls.py
@@ -5,6 +5,6 @@ from appSite.views import registerLogin
 urlpatterns = [
     path('', views.home, name='home'),
     path('games/', views.games_list, name='game_list'),
-    path("register/", views.register_view, name="register"),
-    path("login/", views.login_view, name="login"),
+    path("register/", registerLogin.register_view, name="register"),
+    path("login/", registerLogin.login_view, name="login"),
 ]
