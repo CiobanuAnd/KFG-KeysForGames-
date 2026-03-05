dfabf81a5d5838d2e67c89a53db730260e4fa3e8
Ciobanu Andrei <ciobanuandrei1111@gmail.com>
2026-03-05 11:55:33 +0200

    am facut migratii

--- appSite/models/user.py
+++ appSite/models/user.py
@@ -4,6 +4,7 @@ class User(models.Model):
     username = models.CharField(max_length=50, unique=True)
     email = models.EmailField(unique=True)
     balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    library = models.ManyToManyField('appSite.game', blank=True)
 
     def __str__(self):
         return self.username
