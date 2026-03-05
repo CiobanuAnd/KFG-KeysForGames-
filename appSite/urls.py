0738b5b3820f183fa3ce989e461b020e7b953e69
Daniel <daniel.abel.arcalean@gmail.com>
2026-03-05 11:50:22 +0200

    logout button

--- appSite/urls.py
+++ appSite/urls.py
@@ -3,10 +3,11 @@ from appSite.views import views
 from appSite.views import registerLogin
 from django.contrib.auth import views as auth_views
 
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('games/', views.games_list, name='game_list'),
-    path("register/", registerLogin.register_view, name="register"),
-    path("login/", registerLogin.login_view, name="login"),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', registerLogin.register_view, name='register'),
+    path('login/', registerLogin.login_view, name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='home', redirect_field_name=''), name='logout'),
 ]
