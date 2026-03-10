31ca5fda0f3a98d86e372590d304ba6f3dcb3083
Daniel <daniel.abel.arcalean@gmail.com>
2026-03-10 11:53:59 +0200

    Search bar holding game names

--- appSite/views/views.py
+++ appSite/views/views.py
@@ -4,15 +4,28 @@ from appSite.models import Game
 # Create your views here.
 def home(request):
     from appSite.models.user import User as CustomUser
-    games = Game.objects.all()
-    custom_user = None  # default if not logged in
+    from appSite.models import Game
+
+    query = request.GET.get("q")
+
+    if query:
+        games = Game.objects.filter(title__icontains=query)
+    else:
+        games = Game.objects.all()
+
+    custom_user = None
+
     if request.user.is_authenticated:
         try:
             custom_user = CustomUser.objects.get(username=request.user.username)
         except CustomUser.DoesNotExist:
             custom_user = None
 
-    return render(request, "gameshop/home.html", {"games": games, "custom_user": custom_user})
+    return render(request, "gameshop/home.html", {
+        "games": games,
+        "custom_user": custom_user,
+        "query": query
+    })
 
 
 def games_list(request):
